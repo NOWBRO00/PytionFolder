5015851d5b5fbc19ac6bb7775f27f111782c4ce8
NOWBRO(이재형) <wogud879@gmail.com>
2024-11-22 17:21:30 +0900

    Add files via upload

---
+++ Test01.py
@@ -0,0 +1,23 @@
+def test(N,tower):
+    result=[0]*N
+    stack=[]
+
+    for i in range(N):
+        while stack and stack[-1][0] < tower[i]:
+            stack.pop()
+
+        if stack :
+            result[i]=stack[-1][1]+1
+        else :
+            result[i]=0
+
+        stack.append([tower[i],i])
+
+    return result
+
+
+
+N=5
+tower=[6,9,5,7,4]
+result=test(N,tower)
+print(result)
